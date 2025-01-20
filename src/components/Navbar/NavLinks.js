15a047e7974ac5e6a3307cf3fa573103a53214e5
Maurice <molande.mau@gmail.com>
2025-01-20 20:05:01 +0200

    second commit

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -8,16 +8,13 @@ const NavLinks = () => {
                 About
             </HashLink>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#services">
-                Services
-            </HashLink>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/">
-                Portfolio
+                Our Residences
             </HashLink>
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact#contact">
                 Contact Us
             </HashLink>
             <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo#demo">
-                Demo our products
+                Make a Booking
             </HashLink>
         </>
     )
