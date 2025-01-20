88099032def84bbb36d170abd74f56ce3877fe8e
Maurice <molande.mau@gmail.com>
2025-01-20 23:20:36 +0200

    hero section part 1

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -4,13 +4,13 @@ import { HashLink } from 'react-router-hash-link';
 const NavLinks = () => {
     return (
         <>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#about">
+            <HashLink className="px-4 font-extrabold text-black-500 hover:text-blue-900" smooth to="/#about">
                 About
             </HashLink>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#services">
+            <HashLink className="px-4 font-extrabold text-black-500 hover:text-blue-900" smooth to="/#services">
                 Our Residences
             </HashLink>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact#contact">
+            <HashLink className="px-4 font-extrabold text-black-500 hover:text-blue-900" to="/contact#contact">
                 Contact Us
             </HashLink>
             <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo#demo">
